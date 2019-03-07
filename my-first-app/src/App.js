1740da72b8867f4a1e7ef0d2b6cf09d956f2edf5
Swami Training <swamitraining@MacBook-Pro.local>
2019-03-07 23:21:49 +0530

    Dummy Commit

---
+++ my-first-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Header';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>HELLO WORLD!</h1>
+    </div>
+  )
+};
+
+export default App;
---
+++ my-first-app/src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <p style={{
+                fontSize: '30px',
+                color: '#95a5a6'
+            }}>SAMPLE TITLE</p>
+        </header>
+    );
+};
+
+export default Header;
